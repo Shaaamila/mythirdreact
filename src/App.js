1cdb204d72c620f5b96321c42baa66befd78a136
Shamila <shamila.m09@gmail.com>
2019-06-22 19:29:12 -0700

    import Component, add render(), test data flow in app.js and projects.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
+  
+  render(){
   return (
     <div className="App">
       My App
-      <Projects />
+      // <Projects test="heloo" />
     </div>
   );
 }
-
+}
 
 
 export default App;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
 class Projects extends Component {
+
+	render(){
   return (
     <div className="Projects">
       My Projects
+      {this.props.test}
     </div>
   );
 }
-
+}
 
 
 export default Projects;
