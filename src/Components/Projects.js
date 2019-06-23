311593216ddafcc311a77bdc8b50864c9c99a268
Shamila <shamila.m09@gmail.com>
2019-06-22 19:19:32 -0700

    Created new component

---
+++ src/Components/Projects.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class Projects extends Component {
+  return (
+    <div className="Projects">
+      My Projects
+    </div>
+  );
+}
+
+
+
+export default Projects;
+
+
+//This App.js Component is a placeholder for all other component 
+// and we render them from here
+
