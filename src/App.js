b99592b7b3a257c3c809163415cfdba64aac0174
Shamila <shamila.m09@gmail.com>
2019-06-22 19:35:34 -0700

    creating constructor for dummy data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
-  
+  contructor(){
+    this.state = {
+      project: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce Shopping Cart',
+          category: 'Web Development'
+        }
+      ]
+    }
+  }
   render(){
   return (
     <div className="App">
       My App
-      // <Projects test="heloo" />
+      <Projects />
     </div>
   );
 }
