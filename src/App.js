76d3a42bfc9fda175fed215b5f63a62859eb8180
Shamila <shamila.m09@gmail.com>
2019-06-22 19:13:06 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      My App
     </div>
   );
 }
 
 export default App;
+
+
+//This App.js Component is a placeholder for all other component 
+// and we render them from here
